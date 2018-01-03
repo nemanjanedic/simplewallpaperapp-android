2a2e19ad4f3d06cae3c064cc1d81ae2821ec278c
Nemanja Nedic <n.nedic@levi9.com>
2018-01-03 12:04:15 +0100

    Configured AppCenter builds.

--- app/src/main/java/com/nemanjanedic/simplewallpaperapp/MainActivity.kt
+++ app/src/main/java/com/nemanjanedic/simplewallpaperapp/MainActivity.kt
@@ -5,6 +5,9 @@ import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -16,6 +19,10 @@ class MainActivity : AppCompatActivity() {
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
+        }
+
+        if (!BuildConfig.DEBUG) {
+            AppCenter.start(application, BuildConfig.APP_CENTER_SECRET, Analytics::class.java, Crashes::class.java)
         }
     }
 
